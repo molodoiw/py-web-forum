e2f43ec953d70bf4a74853ac261dc775df7aee18
Smart <molodoiw@mail.ru>
2022-12-11 23:44:57 +0700

    ver 2.0 wth db

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import  Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__) # создание объекта на основе класса Flask# __name - директива для запуска файла
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
+db = SQLAlchemy(app)
 
+
+class Entries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    pretext = db.Column(db.String(500), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Entries %r' % self.id
 
 @app.route('/')
 @app.route('/home')
